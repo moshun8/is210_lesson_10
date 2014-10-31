2c4c485e3b0b94b1e71c22347d3c505f0d16d895
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-31 13:05:15 -0400

    task 1 again

--- boroughs.py
+++ boroughs.py
@@ -3,41 +3,75 @@
 """Task 1 Reading a CSV"""
 
 
-grades = {
-    'A': float(100.0),
-    'B': float(90.0),
-    'C': float(80.0),
-    'D': float(70.0),
-    'F': float(60.0)
+import csv
+import json
+
+
+GRADES = {
+    'A': float(1.0),
+    'B': float(0.9),
+    'C': float(0.8),
+    'D': float(0.7),
+    'F': float(0.6)
 }
+
 
 def get_summary_score(filename):
     '''Makes a dict with ID, Boro, and Grade from CSV file'''
 
     inspect = open(filename)
-    reports = inspect.readline()
+    report = csv.reader(inspect)
+    # inspect.readline()
     summary = {}
 
-    for eachline in reports:
-        info = reports.split(',')
-        idnum = info[0]
-        boro = info[1]
-        grade = info[10]
-
-        for thegrade in eachline:
-            if thegrade is 'P' or '' or 'GRADE':
-                continue
+    for line in report:
+        # info = line.split(',')
+        idnum = line[0]
+        boro = line[1]
+        grade = line[10]
+        if grade == 'A' or 'B' or 'C' or 'D' or 'F':
             summary[idnum] = (boro, grade)
-            return summary
-        inspect.close()
+            # return summary
+        else:
+            continue
+    # return summary
+    inspect.close()
 
-            # for boro in summary
-            #     boro.count('Manhattan')
-            #     boro.count('Bronx')
-            #     boro.count('Staten Island')
-            #     boro.count('Queens')
-            #     boro.count('Brooklyn')
-print get_summary_score('inspection_results.csv')
-'''x = set(x)
-y = set(y)
-y.difference(x) # will return whats different in the sets'''
+    byboro = {}
+    mhscore = 0
+    bxscore = 0
+    siscore = 0
+    qnscore = 0
+    bkscore = 0
+
+    for score in summary.itervalues():
+        mh = score[1].count('MANHATTAN')
+        bx = score[1].count('BRONX')
+        si = score[1].count('STATEN ISLAND')
+        qn = score[1].count('QUEENS')
+        bk = score[1].count('BROOKLYN')
+
+        if score[1] is 'MANHATTAN':
+            mhscore += score[1]
+        elif score[1] is 'BRONX':
+            bxscore += score[1]
+        elif score[1] is 'STATEN ISLAND':
+            siscore += score[1]
+        elif score[1] is 'QUEENS':
+            qnscore += score[1]
+        elif score[1] is 'BROOKLYN':
+            bkscore += score[1]
+        else:
+            pass
+
+        byboro = {
+        'MANHATTAN': (mh, float(mhscore)/float(mh)),
+        'BRONX': (bx, float(bxscore)/float(bx)),
+        'STATEN ISLAND': (si, float(siscore)/float(si)),
+        'QUEENS': (qn, float(qnscore)/float(qn)),
+        'BROOKLYN': (bk, float(bkscore)/float(bk))
+        }
+    return byboro
+
+# def get_market_density(filename):
+#     '''docstring'''
