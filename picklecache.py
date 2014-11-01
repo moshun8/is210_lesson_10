2cfa62aea90642e7a365585c1b331afc4b0069ef
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-11-01 15:24:15 -0400

    rename variables and pylint

--- picklecache.py
+++ picklecache.py
@@ -10,48 +10,42 @@ import pickle
 class PickleCache(object):
     '''Pickles are delicious with sandwiches'''
     # __file_path = self.file_path
-    __file_object = None
-    __data = {}
+    _PickleCache__file_object = None
+    _PickleCache__data = {}
 
     def __init__(self, file_path='datastore.pkl'):
-        self.file_path = file_path
-
+        self._PickleCache__file_path = file_path
 
     def set(self, key, value):
         '''Adds pairs to the data dict'''
-        __data[key] = value
-
+        self._PickleCache__data[key] = value
 
     def get(self, key):
         '''Gets key pair from data dict, or returns an error'''
-        if key in self.__data:
+        if key in self._PickleCache__data:
             return key
         else:
             print "Error: No value found for key '{}'".format(key)
 
-
     def delete(self, key):
         '''Deletes a key from the data dict'''
-        if key in self.__data:
-            del self.__data[key]
-
+        if key in self._PickleCache__data:
+            del self._PickleCache__data[key]
 
     def open(self):
         '''Opens file path if it is not empty'''
-        if os.path.exists(self.__file_path):
-            if os.path.getsize(self.__file_path) > 0:
-                self.__data = pickle.load(self.__file_path)
-                self.__file_path.close()
+        if os.path.exists(self._PickleCache__file_path):
+            if os.path.getsize(self._PickleCache__file_path) > 0:
+                self._PickleCache__data = pickle.load(self._PickleCache__file_path)
+                self._PickleCache__file_path.close()
         open(self.__file_path, 'w')
-
 
     def flush(self, reopen=True):
         '''save stored data'''
         pickle.dump(reopen, self.__file_object)
         self.__file_object.close()
-        if reopen == True:
+        if reopen:
             self.open()
-
 
     def close(self):
         '''closes files'''
